f6c4be1f80feea5730681b8f3d8dadcd419cd94b
morpheuschoo01~ <morpheuschoo@gmail.com>
2023-04-13 22:30:15 +0800

    removed space in front of progress bar

--- progress_bar.py
+++ progress_bar.py
@@ -3,4 +3,4 @@
 def progressBar(progress, total):
     percent = 100 * (progress / total)
     bar = '█' * int(percent) + '-' * (100 - int(percent))
-    print(f'\x1b[34;1m |{bar}| {percent:.2f}% \x1b[0m', end="\r")
+    print(f'\x1b[34;1m|{bar}| {percent:.2f}% \x1b[0m', end="\r")
